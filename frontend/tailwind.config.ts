a1ba74118592c99aced005db9c64af5d1188cab2
Christine Phung <z5363653@ad.unsw.edu.au>
2023-09-02 21:04:42 +1000

    added colours

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -8,10 +8,22 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      // backgroundImage: {
+      //   'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
+      //   'gradient-conic':
+      //     'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      // },
+      backgroundColor: {
+        "ll-bg": "#FFFFFF",
+      },
+      colors: {
+        "ll-dark-pink": "#E85D75",
+        "ll-black": "#000000",
+        "ll-pink": "#F8C1C9",
+        "ll-red": "#911B1B",
+        "ll-white": "#FFFFFF",
+        "ll-light-pink": "#FCDADA",
+        "ll-gray": "#D9D9D9",
       },
     },
   },
