fe1306919981831f9ac8e784815d37f8216220a5
Evan Shi <shizongyan2002@gmail.com>
2023-09-04 18:37:14 +1000

    changed providers

--- frontend/src/app/api/auth/[...nextauth]/options.ts
+++ frontend/src/app/api/auth/[...nextauth]/options.ts
@@ -1,15 +1,10 @@
 import type { NextAuthOptions } from 'next-auth'
-import GitHubProvider from 'next-auth/providers/github'
 import CredentialsProvider from 'next-auth/providers/credentials'
 
 export const options: NextAuthOptions = {
     providers: [
-        GitHubProvider({
-            clientId: process.env.GITHUB_ID as string,
-            clientSecret: process.env.GITHUB_SECRET as string,
-        }),
         CredentialsProvider({
-            name: "Credentials",
+            name: "Login",
             credentials: {
                 username: {
                     label: "Username:",
@@ -26,7 +21,54 @@ export const options: NextAuthOptions = {
                 // This is where you need to retrieve user data 
                 // to verify with credentials
                 // Docs: https://next-auth.js.org/configuration/providers/credentials
+
+                // retrieve user from database, compare with credentials
                 const user = { id: "42", name: "Dave", password: "nextauth" }
+
+                if (credentials?.username === user.name && credentials?.password === user.password) {
+                    return user
+                } else {
+                    return null
+                }
+            }
+        }),
+        CredentialsProvider({
+            name: "Register",
+            credentials: {
+                username: {
+                    label: "Username:",
+                    type: "text",
+                    placeholder: "your-cool-username"
+                },
+                email: {
+                    label: "Email:",
+                    type: "text",
+                    placeholder: "your-email"
+                },
+                password: {
+                    label: "Password:",
+                    type: "password",
+                    placeholder: "your-awesome-password"
+                },
+                confirmation: {
+                    label: "Confirmation",
+                    type: "password",
+                    placeholder:"your-awesome-password-again"
+                }
+            },
+            async authorize(credentials) {
+                // This is where you need to retrieve user data 
+                // to verify with credentials
+                // Docs: https://next-auth.js.org/configuration/providers/credentials
+                const user = { id: "42", name: "Dave", password: "nextauth" }
+
+                if (credentials?.password != credentials?.confirmation) {
+                    return null
+                }
+
+                // check if username already exists
+
+                // check password validity
 
                 if (credentials?.username === user.name && credentials?.password === user.password) {
                     return user
