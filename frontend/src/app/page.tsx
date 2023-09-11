450b2828cf7af4ffaeff73f044f117f7fc72a725
Christine Phung <z5363653@ad.unsw.edu.au>
2023-09-11 18:35:23 +1000

    broken infinite scroll :((

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,25 +1,95 @@
-
+"use client";
 // import Image from "next/image";
 import Divider from "@/components/heartDivider"
 import PostCard from "@/components/defaultPostCard"
 import { get, post } from "@/util/request";
 import { getServerSession } from "next-auth/next";
 import { options } from "@/lib/auth"
+import { LoadMore } from "@/components/loadMore";
+import { useEffect, useRef, useState } from "react";
 
 // export is the dunction that makes it to the website?
 export default async function Home() {
-  // const res = await fetch("http://localhost:3030/posts", { method: "GET", cache: "no-store" })
-  // let login_res = await post("/auth/login", {username:"test123", password:"test123"})
+  const postFinishedRef = useRef(false);
+  const indexRef = useRef(0);
+  // const searchCoursesRef = useRef([]);
 
-  // let token = login_res.token
-  // let id = login_res.id
-  // await post("/post", 
-  // {
-  //   Authorization: "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOiJmMDY3N2QxZi1mNWM0LTQ1Y2MtYTMwMC0wY2ZhZTFjNjFkMjIiLCJpYXQiOjE2OTM4MTc2MDcsImV4cCI6MTY5MzkwNDAwN30.BpAC03yLfHuUVGcu-naS9Zn-5KC6ye4jHMF_-uXseD0",
-  //   id: "f0677d1f-f5c4-45cc-a300-0cfae1c61d22"
-  // },
-  // { message: "i cannnot believe this", images: [], anonymous: false, themeId:"3841fa45-0e54-401c-a31b-57676ee8f98c" }, 
-  // ) 
+  const [displayPosts, setDisplayPosts] = useState<object[]>([]);
+
+  const paginationOffset = 25;
+
+  const loadMore = async (index: number) => {
+    const fetchPosts = async () => {
+      let fetchedPosts = [];
+      try {
+        const { posts } = (await get(
+          `/posts?offset=${index}`,
+        )) as any;
+        fetchedPosts = posts;
+      } catch (err) {
+        fetchedPosts = [];
+      }
+      return fetchedPosts;
+    };
+
+    if (window.innerHeight + window.pageYOffset < document.body.offsetHeight) {
+      return;
+    }
+    if (postFinishedRef.current) {
+      return;
+    }
+
+    const posts = await fetchPosts();
+    if (posts.length === 0) {
+      postFinishedRef.current = true;
+      return;
+    }
+
+    setDisplayPosts((prev) => [...prev, ...posts]);
+  };
+
+  useEffect(() => {
+    const resetRefs = () => {
+      postFinishedRef.current = false;
+      indexRef.current = 0;
+    };
+
+    const getDefaultResults = async () => {
+      try {
+        const { posts } = (await get(`/posts?offset=${0}`)) as any;
+        setDisplayPosts(posts);
+        indexRef.current += paginationOffset;
+      } catch (err) {
+        setDisplayPosts([]);
+      }
+    };
+
+    const getInitialDisplayPosts = () => {
+      // if (searchTerm === "") {
+        getDefaultResults();
+      // } else {
+      //   getSearchResults();
+      // }
+    };
+
+    const loadOnScroll = () => {
+      if (
+        window.innerHeight + window.pageYOffset >= document.body.offsetHeight &&
+        !postFinishedRef.current
+      ) {
+        loadMore(indexRef.current);
+        indexRef.current += paginationOffset;
+      }
+    };
+
+    resetRefs();
+    getInitialDisplayPosts();
+
+    window.addEventListener("scroll", loadOnScroll);
+    return () => window.removeEventListener("scroll", loadOnScroll);
+  }, []);
+
+
   let username = "Guest"
   let userId = ""
   // const session = useSession();
@@ -31,19 +101,20 @@ export default async function Home() {
     // console.log(session.user.username)
   }
 
-  let fetchedPosts = [];
-  try {
-    const { posts } = (await get(
-      `/posts?offset=${0}`, { method: "GET", cache: "no-store" }
-    )) as any;
-    fetchedPosts = posts;
-  } catch (err) {
-    fetchedPosts = [];
-  }
+  // let fetchedPosts = [];
+  // try {
+  //   const { posts } = (await get(
+  //     `/posts?offset=${0}`, { method: "GET" }
+  //   )) as any;
+  //   fetchedPosts = posts;
+  // } catch (err) {
+  //   fetchedPosts = [];
+  // }
 
-  fetchedPosts = fetchedPosts.filter((post: any) => {
-    return userId != post.author.userId ? true : false
-  })
+  // fetchedPosts = fetchedPosts.filter((post: any) => {
+  //   return userId != post.author.userId ? true : false
+  // })
+
 
   return (
     <div className="flex flex-col gap-4 m-12">
@@ -56,7 +127,7 @@ export default async function Home() {
       </div>
       <div className="grid gap-6 grid-cols-[repeat(auto-fit,_minmax(220px,_1fr))]">
         {
-          fetchedPosts.map((post: any) => {
+          displayPosts.map((post: any) => {
             return <PostCard 
                       msg={post.message} 
                       textColor={post.theme.textColor}
@@ -71,6 +142,7 @@ export default async function Home() {
                       id={post.postId}/>;
           })
         }
+        {/* <LoadMore /> */}
 
       </div>
 
